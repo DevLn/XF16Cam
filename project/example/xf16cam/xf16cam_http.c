d21a9d3aa8a3c8bd4a1b4159bb6c2273c9433bcd
Gergo Olah <gergox.info@gmail.com>
2026-08-25 19:49:19 +0200

    fix html LED buttons bug

--- project/example/xf16cam/xf16cam_http.c
+++ project/example/xf16cam/xf16cam_http.c
@@ -366,22 +366,24 @@ static void xf16cam_http_page(int fd)
 					  "data.set(button.name,button.value);try{await fetch(form.action,{method:'POST',body:data})}"
 					  "finally{button.disabled=false}return false}</script>"
 					  );
+	int led_on = xf16cam_board_get_led_on();
 	length = snprintf(dynamic, sizeof(dynamic),
 	                  "<form class=led method=post action=/api/led onsubmit='return submitLed(event,this)'>"
 	                  "<button name=led_on value=%s>"
-					  "Turn LED %s</button> "
-					  , xf16cam_board_get_led_on() ? "false" : "true"
-					  , xf16cam_board_get_led_on() ? "off" : "on"
-	                  "</form>");
+					  "Turn LED %s</button></form>"
+					  , led_on ? "'false'" : "'true'"
+					  , led_on ? "off" : "on"
+	                  );
 	xf16cam_http_send_all(fd, dynamic, length);
 	//Add IR LED control button for PTZ version
+	int ir_led_on = xf16cam_board_get_ir_led_on();
 	length = snprintf(dynamic, sizeof(dynamic),
 	                  "<form class=led method=post action=/api/ir_led onsubmit='return submitLed(event,this)'>"
 	                  "<button name=ir_led_on value=%s>"
-					  "Turn IR LED %s</button> "
-					  , xf16cam_board_get_ir_led_on() ? "false" : "true"
-					  , xf16cam_board_get_ir_led_on() ? "off" : "on"
-	                  "</form>");
+					  "Turn IR LED %s</button></form>"
+					  , ir_led_on ? "'false'" : "'true'"
+					  , ir_led_on ? "off" : "on"
+	                  );
 	xf16cam_http_send_all(fd, dynamic, length);
 	//Add PTZ control buttons for PTZ version
 	XF16CAM_HTTP_SEND_LITERAL(fd,
@@ -950,8 +952,7 @@ static int xf16cam_http_handle(int fd)
 		xf16cam_http_led_json(fd);
 	} else if (strcmp(method, "GET") == 0 && strcmp(path, "/api/ir_led") == 0) {
 		xf16cam_http_ir_led_json(fd);
-	}
-	else if (strcmp(method, "POST") == 0 && strcmp(path, "/api/power") == 0) {
+	} else if (strcmp(method, "POST") == 0 && strcmp(path, "/api/power") == 0) {
 		xf16cam_http_power_json(fd);
 	} else if (strcmp(method, "POST") == 0 && strcmp(path, "/api/wifi") == 0) {
 		int invalid = xf16cam_form_value(body, "ssid", ssid, sizeof(ssid)) != 0 ||
